daf4b42a54cbf086e4fe6735df831c2657bc9b9e
Andrew Berman <andrew.berman@dewr.gov.au>
2024-04-07 09:08:48 +1000

    Add project files.

---
+++ EarlyXrm.PacHelper/Commands/PacSolutionSync.cs
@@ -0,0 +1,55 @@
+﻿namespace EarlyXrm.PacHelper.Commands
+{
+    using EnvDTE;
+    using EnvDTE80;
+    using Microsoft;
+    using Microsoft.VisualStudio.Shell;
+    using System;
+    using System.ComponentModel.Design;
+    using System.Linq;
+    using System.Windows.Forms;
+    using Task = System.Threading.Tasks.Task;
+
+    internal sealed class PacSolutionSync
+    {
+        public static PacSolutionSync Instance
+        {
+            get;
+            private set;
+        }
+
+        private static DTE2 _dte;
+
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            var commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Assumes.Present(commandService);
+
+            _dte = await package.GetServiceAsync(typeof(DTE)) as DTE2;
+            Assumes.Present(_dte);
+
+            var commandId = new CommandID(PackageGuids.guidPacHelperPackageCmdSet, PackageIds.cmdidPacSolutionSync);
+            var command = new OleMenuCommand(Execute, commandId);
+            command.BeforeQueryStatus += Command_BeforeQueryStatus;
+            commandService.AddCommand(command);
+        }
+
+        private static void Command_BeforeQueryStatus(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            var command = (OleMenuCommand)sender;
+            var activeDocument = ((Array)_dte.ToolWindows.SolutionExplorer.SelectedItems).Cast<UIHierarchyItem>().FirstOrDefault();
+            var selectedPath = ((ProjectItem)activeDocument.Object).FileNames[1];
+
+            command.Visible = command.Enabled = selectedPath?.EndsWith(".cs") ?? false;
+        }
+
+        private static void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            MessageBox.Show("sdfdsf");
+        }
+    }
+}
---
+++ EarlyXrm.PacHelper/VSPackage.cs
@@ -0,0 +1,26 @@
+﻿namespace EarlyXrm.PacHelper
+{
+    using Microsoft.VisualStudio;
+    using Microsoft.VisualStudio.Shell;
+    using System;
+    using System.Runtime.InteropServices;
+    using System.Threading;
+    using Task = System.Threading.Tasks.Task;
+
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(PackageGuids.guidPacHelperPackageString)]
+    [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionHasSingleProject_string, PackageAutoLoadFlags.BackgroundLoad)]
+    public sealed class PacHelperPackage : AsyncPackage
+    {
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            
+            await VSCommandTable.InitializeAsync(this);
+            await Commands.PacSolutionSync.InitializeAsync(this);
+            await Commands.org_select.InitializeAsync(this);
+        }
+    }
+}
