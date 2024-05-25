f17a141783c4871f9803dd8caa316c79b7fc1fca
Andrew Berman <andrew.berman@dewr.gov.au>
2024-05-25 22:58:01 +1000

    Update pac clone

--- EarlyXrm.PacHelper/Commands/PacSolutionClone.ps1
+++ EarlyXrm.PacHelper/Commands/PacSolutionClone.ps1
@@ -15,29 +15,54 @@ $solutionName = read-host -Prompt "Solution unique name"
 
 cd $directory
 
+#Clone solution using pac tool
 pac solution clone -n $solutionName
 
 Remove-Item .\$solutionName\.gitignore
 
-$content = [xml](gc $ItemPath);
+# uncomment "Solution Package Type" settings and set to both
+$cdsprojPath = "$solutionName\$solutionName.cdsproj"
+
+$cdsprojContent = Get-Content $cdsprojPath -Raw
+
+$settings = "<!--\s+<PropertyGroup>\s+<SolutionPackageType>Managed</SolutionPackageType>\s+<SolutionPackageEnableLocalization>false</SolutionPackageEnableLocalization>\s+</PropertyGroup>\s+-->"
+
+$newSettings = @"
+<PropertyGroup>
+    <SolutionPackageType>Both</SolutionPackageType>
+    <SolutionPackageEnableLocalization>false</SolutionPackageEnableLocalization>
+  </PropertyGroup>
+"@
+
+$cdsprojContent = $cdsprojContent -replace $settings, $newSettings
+
+Set-Content -Value $cdsprojContent -Path $cdsprojPath
+
+# Include cdsproj file in solution
+$content = [xml](gc $ItemPath)
 
 $configs = [xml]"<ItemGroup>
   <None Include=`"$solutionName\$solutionName.cdsproj`" />
-</ItemGroup>
-"
-
-$importNode = $content.ImportNode($configs.DocumentElement, $true)
-
-$target = [xml]"<Target Name=`"PostBuild`" AfterTargets=`"PostBuildEvent`">
-  <Exec Command=`"dotnet build $solutionName.cdsproj -p:ProjectDir='`$(ProjectDir)'`" WorkingDirectory=`"`$(ProjectDir)/$solutionName`" />
-</Target>
-"
-
-$importTarget = $content.ImportNode($target.DocumentElement, $true)
+</ItemGroup>"
 
 $project = $content.Project
 
+$importNode = $content.ImportNode($configs.DocumentElement, $true)
+
 $project.AppendChild($importNode)
-$project.AppendChild($importTarget)
+
+# Add cdsproj post build command
+$postBuild = "<Exec Command=`"dotnet build $solutionName.cdsproj -p:ProjectDir='`$(ProjectDir)'`" WorkingDirectory=`"`$(ProjectDir)/$solutionName`" />"
+
+$postBuildNode = $project.Target | ? { $_.AfterTargets -eq "PostBuildEvent" }
+if ($null -ne $postBuildNode){
+	$target = [xml]$postBuild
+	$importTarget = $content.ImportNode($target.DocumentElement, $true)
+	$postBuildNode.AppendChild($importTarget)
+} else {
+	$target = [xml]"<Target Name=`"PostBuild`" AfterTargets=`"PostBuildEvent`">$postBuild</Target>"
+	$importTarget = $content.ImportNode($target.DocumentElement, $true)
+	$project.AppendChild($importTarget)
+}
 
 $content.Save($ItemPath)
